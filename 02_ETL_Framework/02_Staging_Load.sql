f751165758947b44ccdddac9491334175f139337
ifelv <101523664+ifelv@users.noreply.github.com>
2026-02-03 11:07:36 +0200

    REFACTOR: Removed @json variable. Used direct CROSS APPLY for efficiency.

--- 02_ETL_Framework/02_Staging_Load.sql
+++ 02_ETL_Framework/02_Staging_Load.sql
@@ -14,22 +14,8 @@ BEGIN
     DECLARE @RowsInserted INT;
     DECLARE @DynamicSQL NVARCHAR(MAX);
 
-    INSERT INTO ETLLog.RunStep
-    (
-        Run_ID,
-        Step_Name,
-        Target_Table,
-        Start_Time,
-        Step_Status
-    )
-    VALUES
-    (
-        @RunID,
-        @StepName,
-        'Staging.Staging_Customer',
-        SYSUTCDATETIME(),
-        'STARTED'
-    );
+    INSERT INTO ETLLog.RunStep (Run_ID, Step_Name, Target_Table, Start_Time, Step_Status)
+    VALUES (@RunID, @StepName, 'Staging.Staging_Customer', SYSUTCDATETIME(), 'STARTED');
 
     SET @RunStepID = SCOPE_IDENTITY(); 
 
@@ -39,94 +25,52 @@ BEGIN
 
         BEGIN TRAN;
 
+        -- REFACTOR: Removed @json variable. Used direct CROSS APPLY for efficiency.
         SET @DynamicSQL = N'
-        DECLARE @json NVARCHAR(MAX);
-        SELECT @json = BulkColumn FROM OPENROWSET
-	        (BULK''' + @FilePath + ''', 
-		    SINGLE_CLOB) AS J
-
         INSERT INTO Staging.Staging_Customer (
-            Run_ID,
-            CLIENTNUM,
-            Attrition_Flag,
-            Customer_Age,
-            Gender_Code,
-            Dependent_count,
-            Education_Level,
-            Marital_Status,
-            Income_Category,
-            Card_Type,
-            Months_on_book,
-            Total_Relationship_Count,
-            Months_Inactive_12_mon,
-            Contacts_Count_12_mon,
-            Credit_Limit,
-            Total_Revolving_Bal,
-            Avg_Open_To_Buy,
-            Total_Amt_Chng_Q4_Q1,
-            Total_Trans_Amt,
-            Total_Trans_Ct,
-            Total_Ct_Chng_Q4_Q1,
-            Avg_Utilization_Ratio,
-            NaiveBayesScore_1,
-            NaiveBayesScore_2,
-            Source_File_Name,
-            Load_Date
+            Run_ID, CLIENTNUM, Attrition_Flag, Customer_Age, Gender_Code, 
+            Dependent_count, Education_Level, Marital_Status, Income_Category, 
+            Card_Type, Months_on_book, Total_Relationship_Count, Months_Inactive_12_mon, 
+            Contacts_Count_12_mon, Credit_Limit, Total_Revolving_Bal, Avg_Open_To_Buy, 
+            Total_Amt_Chng_Q4_Q1, Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1, 
+            Avg_Utilization_Ratio, NaiveBayesScore_1, NaiveBayesScore_2, 
+            Source_File_Name, Load_Date
         )
-        SELECT
-            @RunID,
-            CLIENTNUM,
-            Attrition_Flag,
-            Customer_Age,
-            Gender_Code,
-            Dependent_count,
-            Education_Level,
-            Marital_Status,
-            Income_Category,
-            Card_Type,
-            Months_on_book,
-            Total_Relationship_Count,
-            Months_Inactive_12_mon,
-            Contacts_Count_12_mon,
-            Credit_Limit,
-            Total_Revolving_Bal,
-            Avg_Open_To_Buy,
-            Total_Amt_Chng_Q4_Q1,
-            Total_Trans_Amt,
-            Total_Trans_Ct,
-            Total_Ct_Chng_Q4_Q1,
-            Avg_Utilization_Ratio,
-            NaiveBayesScore_1,
-            NaiveBayesScore_2,
-            @SourceFileName,
-            SYSUTCDATETIME()
-        FROM OPENJSON(@json)
+        SELECT 
+            @RunID, CLIENTNUM, Attrition_Flag, Customer_Age, Gender_Code, 
+            Dependent_count, Education_Level, Marital_Status, Income_Category, 
+            Card_Type, Months_on_book, Total_Relationship_Count, Months_Inactive_12_mon, 
+            Contacts_Count_12_mon, Credit_Limit, Total_Revolving_Bal, Avg_Open_To_Buy, 
+            Total_Amt_Chng_Q4_Q1, Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1, 
+            Avg_Utilization_Ratio, NaiveBayesScore_1, NaiveBayesScore_2, 
+            @SourceFileName, SYSUTCDATETIME()
+        FROM OPENROWSET (BULK ''' + @FilePath + ''', SINGLE_CLOB) AS J
+        CROSS APPLY OPENJSON(BulkColumn) -- <--- This reads directly from the file content
         WITH(
-            CLIENTNUM                  nvarchar(max) ''$.CLIENTNUM'',
-            Attrition_Flag             nvarchar(max) ''$.Attrition_Flag'',
-            Customer_Age               nvarchar(max) ''$.Customer_Age'',
-            Gender_Code                nvarchar(max) ''$.Gender_Code'',
-            Dependent_count            nvarchar(max) ''$.Dependent_count'',
-            Education_Level            nvarchar(max) ''$.Education_Level'',
-            Marital_Status             nvarchar(max) ''$.Marital_Status'',
-            Income_Category            nvarchar(max) ''$.Income_Category'',
-            Card_Type                  nvarchar(max) ''$.Card_Type'',
-            Months_on_book             nvarchar(max) ''$.Months_on_book'',
-            Total_Relationship_Count   nvarchar(max) ''$.Total_Relationship_Count'',
-            Months_Inactive_12_mon     nvarchar(max) ''$.Months_Inactive_12_mon'',
-            Contacts_Count_12_mon      nvarchar(max) ''$.Contacts_Count_12_mon'',
-            Credit_Limit               nvarchar(max) ''$.Credit_Limit'',
-            Total_Revolving_Bal        nvarchar(max) ''$.Total_Revolving_Bal'',
-            Avg_Open_To_Buy            nvarchar(max) ''$.Avg_Open_To_Buy'',
-            Total_Amt_Chng_Q4_Q1       nvarchar(max) ''$.Total_Amt_Chng_Q4_Q1'',
-            Total_Trans_Amt            nvarchar(max) ''$.Total_Trans_Amt'',
-            Total_Trans_Ct             nvarchar(max) ''$.Total_Trans_Ct'',
-            Total_Ct_Chng_Q4_Q1        nvarchar(max) ''$.Total_Ct_Chng_Q4_Q1'',
-            Avg_Utilization_Ratio      nvarchar(max) ''$.Avg_Utilization_Ratio'',
-            NaiveBayesScore_1          nvarchar(max) ''$.NaiveBayesScore_1'',
-            NaiveBayesScore_2          nvarchar(max) ''$.NaiveBayesScore_2''
-        );
-        ';
+            CLIENTNUM                  nvarchar(max),
+            Attrition_Flag             nvarchar(max),
+            Customer_Age               nvarchar(max),
+            Gender_Code                nvarchar(max),
+            Dependent_count            nvarchar(max),
+            Education_Level            nvarchar(max),
+            Marital_Status             nvarchar(max),
+            Income_Category            nvarchar(max),
+            Card_Type                  nvarchar(max),
+            Months_on_book             nvarchar(max),
+            Total_Relationship_Count   nvarchar(max),
+            Months_Inactive_12_mon     nvarchar(max),
+            Contacts_Count_12_mon      nvarchar(max),
+            Credit_Limit               nvarchar(max),
+            Total_Revolving_Bal        nvarchar(max),
+            Avg_Open_To_Buy            nvarchar(max),
+            Total_Amt_Chng_Q4_Q1       nvarchar(max),
+            Total_Trans_Amt            nvarchar(max),
+            Total_Trans_Ct             nvarchar(max),
+            Total_Ct_Chng_Q4_Q1        nvarchar(max),
+            Avg_Utilization_Ratio      nvarchar(max),
+            NaiveBayesScore_1          nvarchar(max),
+            NaiveBayesScore_2          nvarchar(max)
+        );';
 
         EXEC sp_executesql @DynamicSQL, 
             N'@RunID BIGINT, @SourceFileName NVARCHAR(MAX)', 
@@ -137,23 +81,13 @@ BEGIN
         COMMIT;
         
         UPDATE ETLLog.RunStep
-        SET
-            Rows_Inserted = @RowsInserted,
-            End_Time = SYSUTCDATETIME(),
-            Step_Status = 'SUCCESS'
+        SET Rows_Inserted = @RowsInserted, End_Time = SYSUTCDATETIME(), Step_Status = 'SUCCESS'
         WHERE RunStep_ID = @RunStepID;
 
     END TRY
     BEGIN CATCH
         IF XACT_STATE() <> 0 ROLLBACK;
-
-        UPDATE ETLLog.RunStep
-        SET
-            Step_Status = 'FAILED',
-            ErrorMessage = ERROR_MESSAGE(),
-            End_Time = SYSUTCDATETIME()
-        WHERE RunStep_ID = @RunStepID;
-        
+        UPDATE ETLLog.RunStep SET Step_Status = 'FAILED', ErrorMessage = ERROR_MESSAGE(), End_Time = SYSUTCDATETIME() WHERE RunStep_ID = @RunStepID;
         THROW;
     END CATCH;
 END;
